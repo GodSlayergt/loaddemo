2ac5a5e16828b21079ed64a9ff18ba69c0a66046
Gaurav Tiwari <gautiwar6@publicisgroupe.net>
2021-11-19 16:12:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <my-component heading="CEI Widget"></my-component>
+    </div>
+  );
+}
+
+export default App;
